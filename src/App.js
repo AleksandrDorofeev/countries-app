0d555a0151846b66985a5c6a2e7b8d80ec165dc8
Aleksandr <adorofeev@team.amocrm.com>
2019-01-17 08:57:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { CSSTransitionGroup } from 'react-transition-group'
+
 class App extends Component {
+  state = {
+    timerType: "first"
+  };
+
+  handleCLick = () => {
+    this.setState({
+      timerType: "second"
+    })
+  }
+  handleCLick2 = () => {
+    this.setState({
+      timerType: "third"
+    })
+  }
+  handleCLick3 = () => {
+    this.setState({
+      timerType: "first"
+    })
+  }
+
   render() {
+    const { timerType } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React!
-          </a>
-        </header>
+        <button onClick={this.handleCLick}>second</button>
+        <button onClick={this.handleCLick2}>third</button>
+        <button onClick={this.handleCLick3}>first</button>
+        {/* <div className="PomodoroBackground" /> */}
+          {timerType === "first"  ? (
+            <div className="PomodoroBackground" />
+          ) : (
+              ""
+            )}
+            {timerType === "second"  ? (
+            <div className="RestBackground" />
+          ) : (
+              ""
+            )}
+            {timerType === "third"  ? (
+            <div className="RestBackground2" />
+          ) : (
+              ""
+            )}
+        <CSSTransitionGroup
+          transitionName="gradientTransition"
+          transitionEnterTimeout={1000}
+          transitionLeaveTimeout={500}
+          // in={timerType}
+          // appear={true}
+        >
+         {timerType === "first"  ? (
+            <div className="PomodoroBackground" />
+          ) : (
+              ""
+            )}
+          {timerType === "second"  ? (
+            <div className="RestBackground" />
+          ) : (
+              ""
+            )}
+          {timerType === "third"  ? (
+            <div className="RestBackground2" />
+          ) : (
+              ""
+            )}
+        </CSSTransitionGroup>
       </div>
     );
   }
